b01a572898e8321167a76046bc3d99c83521befb
serhat5252 <serhatozturk5252@gmail.com>
2025-07-24 12:43:12 +0300

    İlk commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-  isAdmin: { type: Boolean, default: false }
+  isAdmin: { type: Boolean, default: false 
 });
 
 userSchema.pre("save", async function (next) {
