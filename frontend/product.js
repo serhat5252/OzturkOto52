26ae1c51c2e138ef4187a387e92634b2cfd20c06
serhat5252 <serhatozturk5252@gmail.com>
2025-07-31 13:13:03 +0300

    İlk commit

--- frontend/product.js
+++ frontend/product.js
@@ -192,7 +192,7 @@ document.getElementById("clearBtn")?.addEventListener("click", () => {
   document.getElementById("onlyCriticalStock").checked = false;
 
   renderList([]);           // Arama kutusunu temizleyince boş liste göster
-  populateFilterOptions();  // Seçenekleri güncelle
+  // populateFilterOptions();  // Seçenekleri güncelle
 });
 
 function applyFilters() {
@@ -213,31 +213,13 @@ function applyFilters() {
 }
 
 // Filtre dropdownlarını doldur
-function populateFilterOptions() {
-  const catSel = document.getElementById("filterCategory");
-  const brandSel = document.getElementById("filterBrand");
-  const typeSel = document.getElementById("filterType");
 
-  // Benzersiz verileri filtrele
-  const categories = [...new Set(products.map(p => p.category).filter(Boolean))];
-  const brands = [...new Set(products.map(p => p.brand).filter(Boolean))];
-  const types = [...new Set(products.map(p => p.type).filter(Boolean))];
-
-  // Seçenekleri dolduran yardımcı fonksiyon
-  const fill = (select, items) => {
-    select.innerHTML = `<option value="">Tümü</option>` + items.map(i => `<option>${i}</option>`).join("");
-  };
-
-  fill(catSel, categories);
-  fill(brandSel, brands);
-  fill(typeSel, types);
-}
 
 
 // Sayfa Yüklendiğinde
 document.addEventListener("DOMContentLoaded", async () => {
   await fetchProducts();          // Ürünleri al
   renderList([]);                // Sayfa açıldığında boş liste göster
-  populateFilterOptions();       // Filtreleri doldur
+  // populateFilterOptions();       // Filtreleri doldur
 });
 
