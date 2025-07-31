3cc6fc222e7b7d095ddb0f2f27b9735be755ba46
serhat5252 <serhatozturk5252@gmail.com>
2025-07-31 12:38:20 +0300

    İlk commit

--- frontend/product.js
+++ frontend/product.js
@@ -181,11 +181,7 @@ document.getElementById("filterBtn")?.addEventListener("click", async () => {
   applyFilters();         // Sonra filtrele
 });
 document.getElementById("clearBtn")?.addEventListener("click", () => {
-  document.getElementById("filterKeyword")?.addEventListener("keydown", async (e) => {
-  if (e.key === "Enter") {
-    await fetchProducts();
-    applyFilters();
-  }
+  document.getElementById("filterKeyword").value = "";
   document.getElementById("filterCategory").value = "";
   document.getElementById("filterBrand").value = "";
   document.getElementById("filterType").value = "";
